1c980982a527f31de3e4b9c22d5efacab5ea375b
SilvesterChiao <SilvesterChiao@foxmail.com>
2018-07-27 10:37:56 +0800

    '修改配置重新打包'

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -9,9 +9,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
-var env = process.env.NODE_ENV === 'testing'
-    ? require('../config/test.env')
-    : config.build.env
+var env =
+    process.env.NODE_ENV === 'testing'
+        ? require('../config/test.env')
+        : config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
     module: {
@@ -52,15 +53,16 @@ var webpackConfig = merge(baseWebpackConfig, {
         // you can customize output by editing /index.html
         // see https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
-            filename: process.env.NODE_ENV === 'testing'
-                ? 'index.html'
-                : config.build.index,
+            filename:
+                process.env.NODE_ENV === 'testing'
+                    ? 'index.html'
+                    : config.build.index,
             template: 'index.html',
             inject: true,
             minify: {
                 removeComments: true,
                 collapseWhitespace: true,
-                removeAttributeQuotes: true
+                removeAttributeQuotes: false
                 // more options:
                 // https://github.com/kangax/html-minifier#options-quick-reference
             },
@@ -106,9 +108,7 @@ if (config.build.productionGzip) {
             asset: '[path].gz[query]',
             algorithm: 'gzip',
             test: new RegExp(
-                '\\.(' +
-                config.build.productionGzipExtensions.join('|') +
-                ')$'
+                '\\.(' + config.build.productionGzipExtensions.join('|') + ')$'
             ),
             threshold: 10240,
             minRatio: 0.8
@@ -117,7 +117,8 @@ if (config.build.productionGzip) {
 }
 
 if (config.build.bundleAnalyzerReport) {
-    var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+    var BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+        .BundleAnalyzerPlugin
     webpackConfig.plugins.push(new BundleAnalyzerPlugin())
 }
 
